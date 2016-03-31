96d193b0890a8f5074f2ee9f018991afa29959d7
Jonathon Thompson <Jonathon Thompson>
2016-03-31 16:37:09 -0700

    with canvas

--- gitTabz/app/src/main/java/com/teamnull/thatgoodgood/gittabz/Canvas.java
+++ gitTabz/app/src/main/java/com/teamnull/thatgoodgood/gittabz/Canvas.java
@@ -1,7 +1,32 @@
 package com.teamnull.thatgoodgood.gittabz;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.view.View;
+
+
 /**
  * Created by Jonathon on 3/31/2016.
  */
-public class Canvas {
+public class Canvas extends View {
+
+    Bitmap ball;
+
+
+    public Canvas(Context context) {
+        super(context);
+        ball = BitmapFactory.decodeResource(getResources(), R.drawable.ball);
+    }
+
+    @Override
+    protected void onDraw(android.graphics.Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.drawColor(Color.WHITE);
+        canvas.drawBitmap(ball, (canvas.getWidth()/2), canvas.getHeight()/2, null);
+
+
+    }
+
 }

2cf3af372ffec5546fedd9c20f82819a1c56505e
Danny <dannymthompson94@gmail.com>
2016-03-31 21:10:35 -0700

    Fuck this

--- gitTabz/app/src/main/java/com/teamnull/thatgoodgood/gittabz/Canvas.java
+++ gitTabz/app/src/main/java/com/teamnull/thatgoodgood/gittabz/Canvas.java
@@ -4,12 +4,15 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.graphics.ColorFilter;
 import android.view.View;
-
+import android.graphics.Paint;
+import android.graphics.Rect;
 
 /**
  * Created by Jonathon on 3/31/2016.
  */
+
 public class Canvas extends View {
 
     Bitmap ball;
@@ -21,18 +24,28 @@ public class Canvas extends View {
         changingy = 0;
     }
 
+    Paint p = new Paint();
+
     @Override
-    protected void onDraw(android.graphics.Canvas canvas) {
+    protected void onDraw(android.graphics.Canvas canvas){
         super.onDraw(canvas);
+        p.setStrokeWidth(2);
+        p.setStyle(Paint.Style.FILL);
+        p.setColor(Color.TRANSPARENT);
+        p.setStyle(Paint.Style.STROKE);
+        p.setColor(Color.BLACK);
+
         canvas.drawColor(Color.WHITE);
-        canvas.drawBitmap(ball, (canvas.getWidth()/2), changingy, null);
-        if( changingy < canvas.getHeight() ) {
+        canvas.drawCircle(canvas.getWidth()/4, changingy, 25, p);
+
+        canvas.drawBitmap(ball, (canvas.getWidth() / 2), changingy, null);
+        if (changingy < canvas.getHeight()) {
             changingy += 10;
-        }else {
+        } else {
             changingy = 0;
         }
 
         invalidate();
     }
-
 }
+

83b5a4ca424356232f013e7a7c74436021e83802
Jonathon Thompson <Jonathon Thompson>
2016-03-31 20:55:22 -0700

    beat

--- gitTabz/app/src/main/java/com/teamnull/thatgoodgood/gittabz/MainActivity.java
+++ gitTabz/app/src/main/java/com/teamnull/thatgoodgood/gittabz/MainActivity.java
@@ -1,23 +1,17 @@
 package com.teamnull.thatgoodgood.gittabz;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 
 //NOT SURE ABOUT ALL THIS CRAP BUT HERE SHE IS IN ALL HER GLORY
 
 public class MainActivity extends AppCompatActivity {
 
-    Canvas theView;
+    MyCanvas theView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        theView = new Canvas(this);
+        theView = new MyCanvas(this);
         setContentView(theView);
 
 

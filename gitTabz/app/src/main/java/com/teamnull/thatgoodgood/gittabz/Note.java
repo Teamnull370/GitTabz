3caeeb8afa1b2b978ceb8730fa2836d73d610c73
Zeshadow <Zeyadayoub@hotmail.com>
2016-04-05 19:28:44 -0700

    adds notes and sound class and the sound lib

---
+++ gitTabz/app/src/main/java/com/teamnull/thatgoodgood/gittabz/Note.java
@@ -0,0 +1,36 @@
+package com.teamnull.thatgoodgood.gittabz;
+
+
+import java.lang.Integer;
+/**
+ * Created by Zeyad Ayoub on 4/5/2016.
+ */
+public class Note{
+
+    private Integer _strng =new Integer();
+    private Integer _fret = new Integer();
+
+    private Sound _snd = new Sound();
+
+    public Note(){
+        _strng=-1;
+        _fret=-1;
+
+    }
+
+    public Note(Integer str, Integer frt){
+        _strng=str;
+        _fret=frt;
+        _snd.setTone(_strng,_fret);
+    }
+
+    public void setStrng(Integer str ){ _strng= str; }
+    public void setFret(Integer frt){ _fret = frt;}
+
+    public Integer getStrng(){return strng;}
+    public Integer getFret(){ return _fret}
+
+
+
+
+};
---
+++ gitTabz/app/src/main/java/com/teamnull/thatgoodgood/gittabz/Sound.java
@@ -0,0 +1,25 @@
+package com.teamnull.thatgoodgood.gittabz;
+
+import org.jfugue.player.Player;
+
+import java.lang.Integer;
+/**
+ * Created by Zeyad Ayoub on 4/5/2016.
+ */
+
+public class Sound {
+
+    private String tone= new String();
+   // Player player = new Player();
+
+
+    public Sound (){
+
+    }
+
+    public void setTone(Integer string, Integer fret){
+
+        tone = "A";
+
+    }
+}

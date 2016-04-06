f5226b703d155b05c26ffab5e709e009a936b63e
Danny <dannymthompson94@gmail.com>
2016-04-05 19:59:39 -0800

    Lines drawn in correct position && circles drawn

--- gitTabz/app/src/main/java/com/teamnull/thatgoodgood/gittabz/Canvas.java
+++ gitTabz/app/src/main/java/com/teamnull/thatgoodgood/gittabz/Canvas.java
@@ -16,19 +16,21 @@ import android.graphics.Rect;
 public class Canvas extends View {
 
     Bitmap ball;
-    float changingy;
-
+    float changingx;
     public Canvas(Context context) {
         super(context);
         ball = BitmapFactory.decodeResource(getResources(), R.drawable.ball);
-        changingy = 0;
+        changingx = 0;
     }
 
     Paint p = new Paint();
-
+    Paint q = new Paint();
     @Override
     protected void onDraw(android.graphics.Canvas canvas){
         super.onDraw(canvas);
+        q.setStrokeWidth(5);
+        q.setStyle(Paint.Style.STROKE);
+        q.setColor(Color.BLACK);
         p.setStrokeWidth(2);
         p.setStyle(Paint.Style.FILL);
         p.setColor(Color.TRANSPARENT);
@@ -36,13 +38,25 @@ public class Canvas extends View {
         p.setColor(Color.BLACK);
 
         canvas.drawColor(Color.WHITE);
-        canvas.drawCircle(canvas.getWidth()/4, changingy, 25, p);
+        canvas.drawCircle(changingx, canvas.getHeight() / 8, 25, p);
+        canvas.drawCircle(changingx, canvas.getHeight() / 8 * 2, 25, p);
+        canvas.drawCircle(changingx, canvas.getHeight() / 8 * 3, 25, p);
+        canvas.drawCircle(changingx, canvas.getHeight() / 8 * 4, 25, p);
+        canvas.drawCircle(changingx, canvas.getHeight() / 8 * 5, 25, p);
+        canvas.drawCircle(changingx, canvas.getHeight() / 8 * 6, 25, p);
 
-        canvas.drawBitmap(ball, (canvas.getWidth() / 2), changingy, null);
-        if (changingy < canvas.getHeight()) {
-            changingy += 10;
+        canvas.drawLine(0, canvas.getHeight() / 8, canvas.getWidth(), canvas.getHeight() / 8, q);
+        canvas.drawLine(0, canvas.getHeight()/8 * 2, canvas.getWidth(), canvas.getHeight()/8 * 2, q);
+        canvas.drawLine(0, canvas.getHeight()/8 * 3, canvas.getWidth(), canvas.getHeight()/8 * 3, q);
+        canvas.drawLine(0, canvas.getHeight()/8 * 4, canvas.getWidth(), canvas.getHeight()/8 * 4, q);
+        canvas.drawLine(0, canvas.getHeight()/8 * 5, canvas.getWidth(), canvas.getHeight()/8 * 5, q);
+        canvas.drawLine(0, canvas.getHeight()/8 * 6, canvas.getWidth(), canvas.getHeight()/8 * 6, q);
+        //canvas.drawBitmap(ball, (canvas.getWidth() / 2), changingx, null);
+
+        if (changingx < canvas.getWidth()) {
+            changingx += 5;
         } else {
-            changingy = 0;
+            changingx = 0;
         }
 
         invalidate();

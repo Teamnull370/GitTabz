6f00d239c37658faef154d1817910b5385bb4b62
Danny <dannymthompson94@gmail.com>
2016-04-07 18:43:24 -0800

    Moving circles and transparent red bar

--- gitTabz/app/src/main/java/com/teamnull/thatgoodgood/gittabz/Canvas.java
+++ gitTabz/app/src/main/java/com/teamnull/thatgoodgood/gittabz/Canvas.java
@@ -11,6 +11,7 @@ import android.graphics.Rect;
 
 /**
  * Created by Jonathon on 3/31/2016.
+ * ALL THE COOL PAINT STUFF DONE BY DANNY CUZ HE BE COOL AND SHIT
  */
 
 public class Canvas extends View {
@@ -25,40 +26,53 @@ public class Canvas extends View {
 
     Paint p = new Paint();
     Paint q = new Paint();
+    Paint r = new Paint();
     @Override
     protected void onDraw(android.graphics.Canvas canvas){
         super.onDraw(canvas);
+        r.setStyle(Paint.Style.FILL);
+        r.setStrokeWidth(2);
+        r.setColor(Color.RED);
+
         q.setStrokeWidth(5);
         q.setStyle(Paint.Style.STROKE);
         q.setColor(Color.BLACK);
+
         p.setStrokeWidth(2);
         p.setStyle(Paint.Style.FILL);
         p.setColor(Color.TRANSPARENT);
         p.setStyle(Paint.Style.STROKE);
         p.setColor(Color.BLACK);
 
+        //This creates the white background
         canvas.drawColor(Color.WHITE);
+        //This is the underlying red rectangle
+        canvas.drawRect(canvas.getWidth() / 8, 0, canvas.getWidth() / 8 + 55, canvas.getHeight(), r);
+
+        //These are all of the circles
         canvas.drawCircle(changingx, canvas.getHeight() / 8, 25, p);
         canvas.drawCircle(changingx, canvas.getHeight() / 8 * 2, 25, p);
         canvas.drawCircle(changingx, canvas.getHeight() / 8 * 3, 25, p);
         canvas.drawCircle(changingx, canvas.getHeight() / 8 * 4, 25, p);
         canvas.drawCircle(changingx, canvas.getHeight() / 8 * 5, 25, p);
         canvas.drawCircle(changingx, canvas.getHeight() / 8 * 6, 25, p);
-
+        //This is the Transparent rectangle that goes over the red one.
+        canvas.drawRect(canvas.getWidth() / 8, 0, canvas.getWidth() / 8 + 55, canvas.getHeight(), p);
+        //The bar lines the notes will go on
         canvas.drawLine(0, canvas.getHeight() / 8, canvas.getWidth(), canvas.getHeight() / 8, q);
         canvas.drawLine(0, canvas.getHeight()/8 * 2, canvas.getWidth(), canvas.getHeight()/8 * 2, q);
         canvas.drawLine(0, canvas.getHeight()/8 * 3, canvas.getWidth(), canvas.getHeight()/8 * 3, q);
         canvas.drawLine(0, canvas.getHeight()/8 * 4, canvas.getWidth(), canvas.getHeight()/8 * 4, q);
-        canvas.drawLine(0, canvas.getHeight()/8 * 5, canvas.getWidth(), canvas.getHeight()/8 * 5, q);
+        canvas.drawLine(0, canvas.getHeight() / 8 * 5, canvas.getWidth(), canvas.getHeight() / 8 * 5, q);
         canvas.drawLine(0, canvas.getHeight()/8 * 6, canvas.getWidth(), canvas.getHeight()/8 * 6, q);
-        //canvas.drawBitmap(ball, (canvas.getWidth() / 2), changingx, null);
 
+        //Variable calculates change in x. Higher the # faster the circles move
         if (changingx < canvas.getWidth()) {
             changingx += 5;
         } else {
             changingx = 0;
         }
-
+        //Infinite loop so circles keep moving
         invalidate();
     }
 }

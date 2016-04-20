5bb041a45a89b1efcb3801a8f58fafe65d292f38
student <none@none.com>
2016-04-19 18:42:57 -0700

    Menu & Search Icon

--- gitTabz/app/src/main/java/com/teamnull/thatgoodgood/gittabz/MainActivity.java
+++ gitTabz/app/src/main/java/com/teamnull/thatgoodgood/gittabz/MainActivity.java
@@ -1,27 +1,11 @@
 package com.teamnull.thatgoodgood.gittabz;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.app.ActionBar;
-import android.view.MotionEvent;
-import android.view.View;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View.OnTouchListener;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.app.Activity;
-import android.os.Bundle;
-import java.util.ArrayList;
-import java.util.List;
 import android.widget.Toast;
-import android.support.v7.widget.Toolbar;
-
-import java.lang.reflect.Array;
 
 //NOT SURE ABOUT ALL THIS CRAP BUT HERE SHE IS IN ALL HER GLORY
 
@@ -45,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.settings_menu, menu);
+        getMenuInflater().inflate(R.menu.menu, menu);
         return true;
     }
 
@@ -55,25 +39,35 @@ public class MainActivity extends AppCompatActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         switch (item.getItemId()) {
-            case R.id.action_settings:
+            case R.id.search:
                 Toast.makeText(getApplicationContext(),
-                        "Setting...",
+                        "Settings...",
                         Toast.LENGTH_SHORT).show();
                 break;
+            case R.id.action_settings:
+                Toast.makeText(getApplicationContext(),
+                        "Settings...",
+                        Toast.LENGTH_SHORT).show();
+                break;
+
             case R.id.action_up:
                 Toast.makeText(getApplicationContext(),
-                        "Up...",
+                        "Previous verse",
                         Toast.LENGTH_SHORT).show();
                 break;
             case R.id.action_down:
                 Toast.makeText(getApplicationContext(),
-                        "Down...",
+                        "Next Verse",
                         Toast.LENGTH_SHORT).show();
                 break;
-            case R.id.action_other:
+
+            case R.id.action_ball:
                 Toast.makeText(getApplicationContext(),
                         "Other...",
                         Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.action_other:
+                setContentView(R.layout.easter_egg);
                 break;
             default:
                 Toast.makeText(getApplicationContext(),
